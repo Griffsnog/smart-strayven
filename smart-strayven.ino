d6326d2aa46678160e2ffd875dcfbb7c5f7bfad6
BobLakensi <montgomery.okdaily@gmail.com>
2017-10-27 18:37:17 -0700

    add optimized chrono functions

--- smart-strayven.ino
+++ smart-strayven.ino
@@ -10,7 +10,13 @@
 #define OLED_RESET 4
 #define TEXT_SIZE 8
 
-//pins for button
+#define IR_MAP_TRIP_VAL 95
+#define DART_LEGNTH_FEET 0.2362208333
+
+//io pins
+#define IR_RECEIVER_PIN 0
+
+//pins for buttons
 #define TRIGGER_BTN_PIN 1
 #define RELOAD_BTN_PIN 4
 #define MAG_SZ_TOG_BTN_PIN 5
@@ -30,6 +36,10 @@ byte currentMagSize = 0;  //keep track of the current magazine size
 byte currentAmmo = magSizeArr[currentMagSize];    //keep track of how much ammo there currently is
 byte maxAmmo = magSizeArr[currentMagSize];    //keep track of what the max ammo is, for use when reloading 
 
+double tripTime, exitTime;	//values to keep track of chrono timing
+
+String chronoToPrint, ammoToPrint;		//keep track of what  vals to print
+
 //this code will run when the Arduino turns on
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);    //begin stuff for the display
@@ -38,26 +48,83 @@ void setup() {
 
 //this code loops many times a second
 void loop() {
-  countAmmo();    //count ammo, constantly check for the trigger switch to be pressed to count
+  chrono();    //count ammo, constantly check for the trigger switch to be pressed to count
   reload();       //reload, constantly check for the magazine switch to be pressed/not pressed
   toggleMags();   //toggle between magazine sizes, constanly check for the magazine toggle switch to be pressed
 }
 
 //actually dispaly ammo onto screen
-void displayAmmo (String ammoToDisplay) {
+void displayValues () {
   display.clearDisplay();           //clear the display, so the stuff that was here before is no longer here
   display.setTextSize(TEXT_SIZE);   //set the size of the text
   display.setTextColor(WHITE);      //set the color of text text
-  display.setCursor( (SCREEN_WIDTH/2) - ((ammoToDisplay.length()*2) * (TEXT_SIZE * 1.5)), (SCREEN_HEIGHT/2) - (TEXT_SIZE * 3) );  //center text
-  display.print(ammoToDisplay);     //print the text
+  display.setCursor( (SCREEN_WIDTH/2) - ((ammoToPrint.length()*2) * (TEXT_SIZE * 1.5)), (SCREEN_HEIGHT/2) - (TEXT_SIZE * 3) );  //center text
+  display.print(ammoToPrint);     //print the text
   display.display();                //display the text
 }
 
 //set up ammo to be displayed
 void initDisplayAmmo () {
   //if the ammo to print, current ammo, is less that 10, make it like '01' or '04'  
-  String ammoToDisplay = currentAmmo < 10 ? ("0" + (String)currentAmmo) : (String)currentAmmo;
-  displayAmmo(ammoToDisplay);  //display the text, the ammo
+  String ammoToPrint = currentAmmo < 10 ? ("0" + (String)currentAmmo) : (String)currentAmmo;
+  displayValues();  //display the text, the ammo
+}
+
+void displayChronoValues(String toPrint) {
+    chronoToPrint = toPrint;
+    displayValues();
+}
+
+void initDisplayChronoValues(double fps) {
+    if (fps == -1) {
+        chronoToPrint = "ERR";
+    } else if (fps == -2) {
+        chronoToPrint = "NO FPS";
+    } else {
+        chronoToPrint = ( (String)(fps)  + " fps" );
+    }
+}
+
+void resetChronoVals() {
+    tripTime = -10;
+    exitTime = -10;
+}
+
+double calculateChronoReadings(double firstTime, double secondTime) {
+    if ( (tripTime > -10) && (exitTime > -10) ) {
+        resetChronoVals();
+        return (DART_LEGNTH_FEET) / ((secondTime-firstTime)/1000000.0);
+    }
+}
+
+void chrono() {
+    //when tripped and expecting first value
+    if ((map(analogRead(IR_RECEIVER_PIN), 0, 1023, 0, 100) > IR_MAP_TRIP_VAL) && (tripTime == -10) ) { 
+        tripTime = micros();
+    //when tripped and expecting second value
+    } else if ( (tripTime != -10) && (exitTime == -10) && (map(analogRead(IR_RECEIVER_PIN), 0, 1023, 0, 100) < IR_MAP_TRIP_VAL) )  {
+        exitTime = micros();
+        initDisplayChronoValues(calculateChronoReadings(tripTime, exitTime));
+
+        //count ammo stuff
+        //make sure that the ammo is less than 99 so it doesnt overflow the display
+        //make sure it's in increment mode
+        if ( (magSizeArr[currentMagSize] == 0) && (currentAmmo < 99) ) {
+            currentAmmo++;    //increment ammo
+        
+        //make sure that the ammo is more than 0 so no negative numbers are displayed
+        //make sure it's in increment mode
+        } else if ( (currentAmmo > 0) && (magSizeArr[currentMagSize] != 0) ){
+            currentAmmo--;    //decrement ammo
+        }
+    
+        initDisplayAmmo();    //display the ammo    
+        
+    //when no second value within 1 second
+    } else if ( (tripTime != -10) && (tripTime + 2000000) < micros() ) {
+        resetChronoVals();
+        initDisplayChronoValues(-1);
+    } 
 }
 
 void reload () {
